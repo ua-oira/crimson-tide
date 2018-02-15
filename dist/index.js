ada5c639bf7a5cf165fc7d128044e545279a57b6
Coston Perkins <coston.perkins@ua.edu>
2018-02-15 16:14:27 -0600

    rm reference to footer

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.Footer = exports.Nav = undefined;
+exports.Nav = undefined;
 
 var _react = require('react');
 
@@ -16,4 +16,3 @@ var _Nav2 = _interopRequireDefault(_Nav);
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.Nav = _Nav2.default;
-exports.Footer = Footer;
