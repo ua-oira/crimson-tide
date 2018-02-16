54f001056dce2685a81ee012a9f3c8dcf50c7cd8
Coston Perkins <coston.perkins@ua.edu>
2018-02-16 08:31:01 -0600

    Add logo

--- dist/index.js
+++ dist/index.js
@@ -3,7 +3,7 @@
 Object.defineProperty(exports, "__esModule", {
   value: true
 });
-exports.SmFooter = exports.Nav = undefined;
+exports.ChimeIn = exports.SmFooter = exports.Nav = undefined;
 
 var _react = require('react');
 
@@ -17,7 +17,12 @@ var _SmFooter = require('./SmFooter.js');
 
 var _SmFooter2 = _interopRequireDefault(_SmFooter);
 
+var _chimeIn = require('./logos/chime-in.svg');
+
+var _chimeIn2 = _interopRequireDefault(_chimeIn);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
 exports.Nav = _Nav2.default;
 exports.SmFooter = _SmFooter2.default;
+exports.ChimeIn = _chimeIn2.default;
--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Nav from './Nav.js'
 import SmFooter from './SmFooter.js'
+import ChimeIn from './logos/chime-in.svg'
 
-export { Nav, SmFooter }
+export { Nav, SmFooter, ChimeIn }
