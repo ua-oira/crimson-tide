d67b0b455839eb44e3203e77fe24d1b42ca4992e
Coston Perkins <coston.perkins@ua.edu>
2018-02-18 14:05:30 -0600

    Complete PageTitle Test

--- test/PageTitle.test.js
+++ test/PageTitle.test.js
@@ -4,23 +4,48 @@ import Helmet from 'react-helmet'
 
 describe('PageTitle', () => {
   
-  test('renders the document title via Helmet', () => {
-    const wrapper = shallow(
-      <PageTitle name="Amazing Page" />
-    );
-    console.log(wrapper.find(Helmet))
-    expect(wrapper.find(Helmet).length).toBe(1);
-    expect(wrapper.find(Helmet).prop('title')).toEqual('Amazing Page')
-  });
+  // test('renders the document title via Helmet', () => {
+  //   const wrapper = shallow(
+  //     <PageTitle name="Amazing Page" />
+  //   );
+  //   console.log(wrapper.find(Helmet))
+  //   expect(wrapper.find(Helmet).length).toBe(1);
+  //   expect(wrapper.find(Helmet).prop('title')).toEqual('Amazing Page')
+  // });
 
-  test('renders the document title via Helmet', () => {
+  // test('renders the document title via Helmet', () => {
+  //   const wrapper = shallow(
+  //     <PageTitle name="Amazing Page" />
+  //   );
+  //   console.log(wrapper.find(Helmet))
+  //   expect(wrapper.find(Helmet).length).toBe(1);
+  //   expect(wrapper.find(Helmet).prop('title')).toEqual('Amazing Page')
+  // });
+
+  test('Render PageTitle with all props', () => {
     const wrapper = shallow(
-      <PageTitle name="Amazing Page" />
-    );
-    console.log(wrapper.find(Helmet))
-    expect(wrapper.find(Helmet).length).toBe(1);
-    expect(wrapper.find(Helmet).prop('title')).toEqual('Amazing Page')
-  });
+      <PageTitle 
+        name="Contact"
+        description="foo"
+        keywords="bar"
+        image="baz"
+        site="OIRA"
+  /> 
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
+
+  test('Render PageTitle with noHeader and no site prop', () => {
+    const wrapper = shallow(
+      <PageTitle 
+        name="Contact"
+        description="foo"
+        keywords="bar"
+        image="baz"
+  /> 
+    )
+    expect(wrapper).toMatchSnapshot()
+  })
 
 })
 
