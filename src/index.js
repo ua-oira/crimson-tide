aae948ef65802a1ae12c5a30013487f331acc6a2
Coston Perkins <coston.perkins@ua.edu>
2018-04-17 15:09:22 -0500

    remove styledlink

--- src/index.js
+++ src/index.js
@@ -1,6 +1,5 @@
 import React from 'react'
 import Nav from './Nav.js'
-import StyledLink from './StyledLink.js'
 import PageTitle from './PageTitle.js'
 import Main from './Main.js'
 import SmFooter from './SmFooter.js'
@@ -8,4 +7,4 @@ import ChimeIn from './logos/chime-in.svg'
 import OiraStyles from './styles/oira-styles.css'
 import { Tabs, Tab } from './styles/oira-styles.css'
 
-export { Nav, StyledLink, PageTitle, Main, SmFooter, ChimeIn, OiraStyles, Tabs, Tab }
+export { Nav, PageTitle, Main, SmFooter, ChimeIn, OiraStyles, Tabs, Tab }
