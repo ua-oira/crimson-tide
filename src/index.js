ee7e5aa9810f79491a1577e582058c5a46b248f6
Coston Perkins <coston.perkins@ua.edu>
2018-04-18 14:18:44 -0500

    Add logos

--- src/index.js
+++ src/index.js
@@ -4,7 +4,20 @@ import PageTitle from './PageTitle.js'
 import Main from './Main.js'
 import SmFooter from './SmFooter.js'
 import ChimeIn from './logos/chime-in.svg'
+import GDGD from './logos/gdgd.svg'
+import OIRALogo from './logos/oira-logo.svg'
 import OiraStyles from './styles/oira-styles.css'
 import { Tabs, Tab } from './Tabs.js'
 
-export { Nav, PageTitle, Main, SmFooter, ChimeIn, OiraStyles, Tabs, Tab }
+export {
+  Nav,
+  PageTitle,
+  Main,
+  SmFooter,
+  ChimeIn,
+  GDGD,
+  OIRALogo,
+  OiraStyles,
+  Tabs,
+  Tab,
+}
