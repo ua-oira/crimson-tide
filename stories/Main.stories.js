c06ab1ce2985fc65648deff3b052b26ca138bf96
Coston Perkins <coston.perkins@ua.edu>
2018-04-18 15:17:08 -0500

    Update docs

--- stories/Main.stories.js
+++ stories/Main.stories.js
@@ -19,17 +19,15 @@ storiesOf('Main Wrapper', module).add(
 
       ### Built In Styles
       ~~~js
-        margin:  '1em auto 1em auto',
+        margin:  '1em auto',
         maxWidth: 960,
         minHeight: 500,
         padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0
-      ~~~
     
     `)(() => (
     <Main>
       <h1>A Wrapper</h1>
-      <p>It contains {'<main>'} tags for really good markup.</p>
+      <p>Wraps main contents in {'<main>'} tags and adds margin, max-width, min-height, and padding.</p>
       <p>The content inside the {'<main>'} element should be unique to the document. It should not contain any content that is repeated across documents such as sidebars, navigation links, copyright information, site logos, and search forms.</p>
       <p>Note: There must not be more than one {'<main>'} element in a document. The {'<main>'} element must NOT be a descendant of an {'<article>'}, {'<aside>'}, {'<footer>'}, {'<header>'}, or {'<nav>'} element.</p>
     </Main>
