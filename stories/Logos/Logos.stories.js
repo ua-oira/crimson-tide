47d067e2f4550e79b73402a8e53dcdb511160340
Coston Perkins <coston.perkins@ua.edu>
2018-04-18 14:19:29 -0500

    Add Logo Stories

---
+++ stories/Logos/Logos.stories.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+import { withInfo } from '@storybook/addon-info'
+
+import { ChimeIn, GDGD, OIRALogo } from '../../src/index'
+
+const BG = { backgroundImage: 'linear-gradient(to right, #7474bf, #348ac7)' }
+
+storiesOf('Logos', module).add(
+  'ChimeIn',
+  withInfo(`
+      
+      ~~~js
+      {ChimeIn}
+      ~~~
+    
+    `)(() => <img style={BG} src={ChimeIn} />)
+)
+
+storiesOf('Logos', module).add(
+  'Good Data, Good Decisions',
+  withInfo(`
+      
+      ~~~js
+      {GDGD}
+      ~~~
+    
+    `)(() => <img style={BG} src={GDGD} />)
+)
+
+storiesOf('Logos', module).add(
+  'OIRA Logo',
+  withInfo(`
+      
+      ~~~js
+      {OIRALogo}
+      ~~~
+    
+    `)(() => <img style={BG} src={OIRALogo} />)
+)
