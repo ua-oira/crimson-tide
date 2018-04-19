67a4f2746959a8a7f45075a397da0764bda63cf7
Coston Perkins <coston.perkins@ua.edu>
2018-04-19 09:12:57 -0500

    Add link component

---
+++ src/Link.js
@@ -0,0 +1,24 @@
+import React from 'react'
+import GatsbyLink from 'gatsby-link'
+
+const Link = ({ children, to, ...others }) => {
+
+  const internal = /^\/(?!\/)/.test(to)
+
+  if (internal) {
+    return (
+      <GatsbyLink to={to} {...others}>
+        {children}
+      </GatsbyLink>
+    )
+  }
+  return (
+    <a href={to} target="_blank" rel="noopener" {...others}>
+      {children}
+    </a>
+  )
+}
+
+
+export default Link
+
