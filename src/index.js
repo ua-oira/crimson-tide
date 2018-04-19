4c21627d36197097221b7b4a4130983b36765505
Coston Perkins <coston.perkins@ua.edu>
2018-04-19 16:02:26 -0500

    Add Alair Logo

--- src/index.js
+++ src/index.js
@@ -5,9 +5,11 @@ import NavWithMenu from './Navigation/NavWithMenu.js'
 import PageTitle from './PageTitle.js'
 import Main from './Main.js'
 import SmFooter from './SmFooter.js'
+// Logos
 import ChimeIn from './logos/chime-in.svg'
 import GDGD from './logos/gdgd.svg'
 import OIRALogo from './logos/oira-logo.svg'
+import ALAIR from './logos/alair-logo.svg'
 import OiraStyles from './styles/oira-styles.css'
 import { Tabs, Tab } from './Tabs.js'
 import Link from './Link.js'
@@ -18,9 +20,11 @@ export {
   PageTitle,
   Main,
   SmFooter,
+  // LOGOS
   ChimeIn,
   GDGD,
   OIRALogo,
+  ALAIR,
   OiraStyles,
   Tabs,
   Tab,
