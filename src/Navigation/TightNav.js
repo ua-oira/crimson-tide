a5d9edb955a60a02b4fa04082a9da86f882579b7
Coston Perkins <coston.perkins@ua.edu>
2018-04-19 09:14:20 -0500

    Add TightNav

---
+++ src/Navigation/TightNav.js
@@ -0,0 +1,135 @@
+import React from 'react'
+import styled from 'styled-components'
+import Headroom from 'react-headroom'
+
+import LogoImage from '../logos/oira-logo.svg'
+import Link from '../Link'
+
+class TightNav extends React.Component {
+  render() {
+    return (
+      <nav>
+        <Headroom>
+          <PageWidth>
+            <ContentWidth>
+              <Left>
+                <Logo to="/" title="Home" />
+              </Left>
+              <Right>
+                <StyledLink to="/mission/">{'Mission'}</StyledLink>
+                {' | '}
+                <StyledLink to="/reports/">{'Reports'}</StyledLink>
+                {' | '}
+                <StyledLink to="http://oira.ua.edu/factbook">{'Factbook'}</StyledLink>
+                {' | '}
+                <StyledLink to="/requests/">{'Requests'}</StyledLink>
+                {' | '}
+                <StyledLink to="/staff/">{'Staff'}</StyledLink>
+                {' | '}
+                <StyledLink to="/resources/">{'Resources'}</StyledLink>
+              </Right>
+            </ContentWidth>
+          </PageWidth>
+        </Headroom>
+      </nav>
+    )
+  }
+}
+
+export default TightNav
+
+const PageWidth = styled.header`
+  border-bottom: 1px solid hsla(0, 0%, 0%, 0.2);
+  background: HSLA(0, 0%, 100%, 0.96);
+  color: black;
+`
+const Logo = styled(Link)`
+  display: inline-block;
+  width: 328px;
+  height: 80px;
+  border-bottom: none;
+  background-color: blue;
+  background: url(${LogoImage});
+  background-size: 100% 100%;
+  background-repeat: no-repeat;
+  text-indent: -9999px;
+  z-index: 3;
+  @media print {
+    * {
+      -webkit-print-color-adjust: exact;
+    }
+  }
+`
+const ContentWidth = styled.div`
+  font-size: 0.88em;
+  line-height: 0.88em;
+  display: flex;
+  color: black;
+  flex-flow: row wrap;
+  margin: 0 auto;
+  width: 100%;
+  padding: 0 1.0875rem;
+  max-width: 1148px;
+  justify-content: space-between;
+  align-items: center;
+  @media (max-width: 810px) {
+    flex-flow: column;
+  }
+  @media print {
+    flex-flow: row;
+  }
+`
+const Left = styled.div`
+  float: left;
+  @media print {
+    -webkit-print-color-adjust: exact;
+  }
+`
+const Right = styled.div`
+  text-align: right;
+  color: hsla(0, 0%, 0%, 0.2);
+  cursor: default;
+  float: Right;
+  // padding-bottom: 1em;
+  @media (max-width: 810px) {
+    text-align: center;
+    margin-bottom: 1em;
+  }
+  @media print {
+    display: none;
+  }
+`
+
+const Contact = styled.p`
+  font-size: 12px;
+  line-height: 12px;
+  font-family: 'Space Mono', monospace;
+`
+const StyledLink = styled(Link)`
+  color: black;
+  font-size: 1.1em;
+  text-decoration: none;
+  &:hover {
+    color: #990000;
+  }
+  &:active {
+    font-weight: bold;
+  }
+  @media (max-width: 810px) {
+    line-height: 1.5em;
+  }
+`
+const StyledAnchor = styled.a`
+  color: black;
+  font-size: 1.1em;
+  text-decoration: none;
+  &:hover {
+    color: #990000;
+  }
+  &:active {
+    font-weight: bold;
+  }
+  @media (max-width: 810px) {
+    line-height: 1.5em;
+  }
+`
---
+++ stories/Navigation/TightNav.stories.js
@@ -0,0 +1,45 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+import { withInfo } from '@storybook/addon-info'
+import StoryRouter from 'storybook-react-router'
+
+import { TightNav, ChimeIn } from '../../src/index'
+
+const NavLinks = [
+  { title: 'Planning Group', path: '/planning-group' },
+  { title: 'Steering Committee', path: '/steering-committee' },
+  { title: 'FAQ', path: '/faq' },
+  { title: 'Resources', path: '/resources' },
+]
+
+storiesOf('Navigation', module)
+  .addDecorator(StoryRouter())
+  .add(
+    'TightNav',
+    withInfo(`
+      - A basic navbar. 
+      - Great for quick site spin-up. 
+      - Add nav links via array of objects to the links attribute.
+      
+      ## Usage
+      
+      ~~~js
+      import { Nav, ChimeIn } from 'crimson-tide'
+
+      const NavLinks = [
+        { title: 'Planning Group', path: '/planning-group' },
+        { title: 'Steering Committee', path: '/steering-committee' },
+        { title: 'FAQ', path: '/faq' },
+        { title: 'Resources', path: '/resources' },
+      ]
+
+      <Nav image={ChimeIn} links={NavLinks} />
+      ~~~
+    
+    `)(() => (
+      <TightNav />
+    ))
+  )
