f5203ed1d11b69ae1003737cb8e6f1d5ddf18b6c
Coston Perkins <coston.perkins@ua.edu>
2018-04-20 15:08:55 -0500

    Add NavLink

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 import React from 'react'
+// Navigation
 import Nav from './Navigation/Nav.js'
 import TightNav from './Navigation/TightNav.js'
 import NavWithMenu from './Navigation/NavWithMenu.js'
+import NavLink from './Navigation/NavLink.js'
 import PageTitle from './PageTitle.js'
 import Main from './Main.js'
 import SmFooter from './SmFooter.js'
@@ -16,6 +18,7 @@ import Link from './Link.js'
 
 export {
   Nav,
+  NavLink,
   TightNav,
   PageTitle,
   Main,
