82f465726e36137d525798dfff58ebc7f1766837
Coston Perkins <coston.perkins@ua.edu>
2018-05-03 12:12:08 -0500

    Add Link options

--- src/Link.js
+++ src/Link.js
@@ -1,14 +1,40 @@
 import React from 'react'
 import GatsbyLink from 'gatsby-link'
+import Obfuscate from 'react-obfuscate'
 
 const Link = ({ children, to, ...others }) => {
   const internal = /^\/(?!\/)/.test(to)
+  const phone = /^(\+\d{1,2}\s)?\(?\d{3}\)?[\s.-]?\d{3}[\s.-]?\d{4}$/.test(to)
+  const email = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(
+    to
+  )
+  const map = /^[-+]?([1-8]?\d(\.\d+)?|90(\.0+)?),\s*[-+]?(180(\.0+)?|((1[0-7]\d)|([1-9]?\d))(\.\d+)?)$/.test(
+    to
+  )
 
   if (internal) {
     return (
       <GatsbyLink to={to} {...others}>
         {children}
       </GatsbyLink>
+    )
+  } else if (phone) {
+    return (
+      <Obfuscate tel={to} {...others}>
+        {children}
+      </Obfuscate>
+    )
+  } else if (email) {
+    return <Obfuscate email={to} {...others} />
+  } else if (map) {
+    return (
+      <a
+        href={'http://maps.apple.com/?q=' + to}
+        title="Map coordinates"
+        {...others}
+      >
+        {children}
+      </a>
     )
   }
   return (
---
+++ stories/Link.stories.js
@@ -0,0 +1,33 @@
+import React from 'react'
+
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
+import { linkTo } from '@storybook/addon-links'
+import { Link } from '../src/index'
+import { withInfo } from '@storybook/addon-info'
+import StoryRouter from 'storybook-react-router'
+
+
+
+
+storiesOf('Link', module)
+.addDecorator(StoryRouter())
+.add(
+  'Link',
+  withInfo(`
+      
+      
+      ~~~js
+      <Link />
+      ~~~
+    
+    `)(() => (
+      <div>
+        <Link to="/Boom">React Router link</Link><br/>
+        <Link to="http://coston.io">Regular link, opens new tab</Link><br/>
+        <Link to="305-205-4353">Children not rendered</Link><br/>
+        <Link to="hello@ua.edu">Children not rendered</Link><br/>
+        <Link to="33.2084338,-87.5472800">Map to East Annex</Link><br/>
+      </div>
+    ))
+)
