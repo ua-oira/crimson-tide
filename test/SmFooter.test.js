98425d5a7444a31e4f89dbe9aad89d816e15ec45
Coston Perkins <coston.perkins@ua.edu>
2019-01-22 13:43:19 -0600

    Update tests

--- test/SmFooter.test.js
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-import SmFooter from '../src/SmFooter'
-
-test('Render SmFooter', () => {
-  const wrapper = shallow(<SmFooter />)
-  expect(wrapper).toMatchSnapshot()
-})
